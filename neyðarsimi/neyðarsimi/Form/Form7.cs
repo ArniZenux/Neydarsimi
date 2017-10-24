06a73f2275bde9c8e9235c644380b021b7d63e47
zenarni <zenarni@gmail.com>
2017-10-24 12:49:12 +0000

    Neydarsimi

---
+++ neyðarsimi/neyðarsimi/Form/Form7.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace neyðarsimi
+{
+    public partial class Form7 : Form
+    {
+        #region "Classes"
+        trial clsTrial = new trial();
+        #endregion
+
+        #region "Variable"
+        private Form1 fm1;
+        public int nr;
+        #endregion
+
+        #region "Form7"
+        public Form7(Form1 form)
+        {
+            InitializeComponent();
+            fm1 = form; 
+        }
+        #endregion
+
+        #region "Form load"
+        private void Form7_Load(object sender, EventArgs e)
+        {
+            label1.Text = clsTrial.getDagur();
+        }
+        #endregion
+
+        #region "Button"
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Close();
+            fm1.Close(); 
+        }
+        #endregion 
+    }
+}
---
+++ neyðarsimi/neyðarsimi/classes/trial.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace neyðarsimi
+{
+    class trial : Global
+    {
+        #region "Class"
+        dbConnection clsDatabase = new dbConnection();
+        #endregion
+
+        #region "VARIABLES"
+        public int numer;
+        public string dagur = "arniarni"; 
+        #endregion
+
+        #region "PROPERTIES"
+        public void setDagur(string dagur)
+        {
+            this.dagur = dagur;
+        }
+
+        public void setNumer(int numer)
+        {
+            this.numer = numer;
+        }
+
+        public string getDagur()
+        {
+            return dagur;
+        }
+
+        public int getNumer()
+        {
+            return numer;
+        }
+        #endregion
+
+        #region "Functions"
+        public void haekkunDagur()
+        {
+            numer = 5;
+        }
+
+        public void skraNumerDagur()
+        {
+            string sqlString = "INSERT INTO tblTrial(haekkun) VALUES('" + numer + "')" ;
+            clsDatabase.ExcuteQuery(sqlString); 
+        }
+
+        public void leyfisdagur()
+        {
+            string sqlString = "SELECT haekkun FROM tblTrial;" ;
+            clsDatabase.GetRecord(sqlString);
+
+            while (reader.Read())
+            {
+                
+            }
+            reader.Close();
+        }
+        
+        #endregion 
+    }
+}
